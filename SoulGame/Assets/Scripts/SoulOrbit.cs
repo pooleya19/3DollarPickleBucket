7c0984fb2763f15ab089c4bc5ecb852674f6cfe4
pooleya19 <pooleya19@gmail.com>
2022-02-27 13:32:37 -0500

    Soul Orbit from inventory

--- SoulGame/Assets/Scripts/SoulOrbit.cs
+++ SoulGame/Assets/Scripts/SoulOrbit.cs
@@ -6,10 +6,12 @@ public class SoulOrbit : MonoBehaviour
 {
     public GameObject gameManager;
     EquipmentManager equipmentManager;
+    InventorySystem inventorySystem;
     int numEquipmentSlots;
     public float xRadius = 0.5f;
     public float yRadius = 0.3f;
     public float frequency = 0.5f;
+    public float scale = 0.4f;
 
     GameObject empty;
     List<GameObject> orbitingSouls = new List<GameObject>();
@@ -19,6 +21,7 @@ public class SoulOrbit : MonoBehaviour
     void Start()
     {
         equipmentManager = gameManager.GetComponent<EquipmentManager>();
+        inventorySystem = gameManager.GetComponent<InventorySystem>();
         empty = new GameObject();
     }
 
@@ -26,28 +29,31 @@ public class SoulOrbit : MonoBehaviour
     void Update()
     {
         numEquipmentSlots = equipmentManager.currentEquipment.Length;
-        int numEquippedSouls = equipmentManager.getNumEquippedSouls();
+        int numEquippedSouls = inventorySystem.inventory.Count; //equipmentManager.getNumEquippedSouls();
+        Debug.Log("Num: " + numEquippedSouls);
         if (numOrbitingSouls != numEquippedSouls)
         {
+            Debug.Log("Fixing");
             //Update orbitting souls
-            for (int i = 0; i < orbitingSouls.Capacity; i++) Destroy(orbitingSouls[i]);
+            for (int i = 0; i < orbitingSouls.Count; i++) Destroy(orbitingSouls[i]);
             orbitingSouls.Clear();
 
-            for (int i = 0; i < numEquipmentSlots; i++)
+            for (int i = 0; i < numEquippedSouls; i++)
             {
-                Equipment equipment = equipmentManager.currentEquipment[i];
-                GameObject soul = Instantiate(empty, Vector3.zero, Quaternion.identity);
+                InventoryItemData equipment = inventorySystem.inventory[i].data;
+                GameObject soul = Instantiate(empty, Vector3.zero, Quaternion.identity, transform);
+                soul.transform.localScale = new Vector3(scale, scale, scale);
                 SpriteRenderer spriteRenderer = soul.AddComponent<SpriteRenderer>();
                 spriteRenderer.sprite = equipment.icon;
                 spriteRenderer.color = equipment.color;
                 orbitingSouls.Add(soul);
             }
         }
-
+        Debug.Log("Equipped: " + numEquippedSouls);
         for (int i = 0; i < numEquippedSouls; i++)
         {
             GameObject soul = orbitingSouls[i];
-            float angle = frequency * Time.time + 2 * Mathf.PI / numEquippedSouls;
+            float angle = frequency * Time.time + 2 * Mathf.PI / numEquippedSouls * i;
             soul.transform.localPosition = new Vector3(xRadius * Mathf.Cos(angle), yRadius * Mathf.Sin(angle), 0);
         }
     }
