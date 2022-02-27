76c26d8a9e48fd05549d1029374d364f160a63a6
pooleya19 <pooleya19@gmail.com>
2022-02-26 20:14:14 -0500

    Clutch save?

--- SoulGame/Assets/Scripts/Enemy/RedEnemy.cs
+++ SoulGame/Assets/Scripts/Enemy/RedEnemy.cs
@@ -4,38 +4,119 @@ using UnityEngine;
 
 public class RedEnemy : EnemyBehavior
 {
-    public float rangeClose = 2.0f;
-    public Vector2 HPRange = new Vector2(5, 15);
+    public float rangeAttackPlayer = 1.0f;
+    public float rangeFindPlayer = 2.0f;
+    public float rangeLosePlayer = 4.0f;
+
+    public Vector2 idleFidgetDelayRange = new Vector2(1, 8);
+    public Vector2 idleFidgetDistanceRange = new Vector2(0, 2);
+    public float minFidgetThreshold = 0.1f;
+    public float maxDistanceFromSpawnPoint = 2.0f;
+
+    float lastIdleFidgetTime = 0;
+    float idleFidgetDelay = 0;
+    Vector2 targetPosition;
+
+    SpriteRenderer spriteRenderer;
+
+    public float HP;
+    public float ATK;
+    public float MVSP;
+    public float ATKSP;
 
     public override void action_START()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
         HPRange = new Vector2(5, 15);
         ATKRange = new Vector2(3, 8);
         MVSPRange = new Vector2(1, 3);
         ATKSPRange = new Vector2(0.2f, 0.4f);
-        //Range = 2.0f;
+
+        HP = Random.Range(HPRange.x, HPRange.y);
+        ATK = Random.Range(ATKRange.x, HPRange.y);
+        MVSP = Random.Range(MVSPRange.x, MVSPRange.y);
+        ATKSP = Random.Range(ATKSPRange.x, ATKSPRange.y);
+
+        targetPosition = spawnPoint;
     }
 
     public override EnemyState getState()
     {
         Vector2 playerPosition = new Vector2(playerTransform.position.x, playerTransform.position.y);
-
-
-        return EnemyState.IDLE;
+        Vector2 position = new Vector2(transform.position.x, transform.position.y);
+        float distance = Vector2.Distance(position, playerPosition);
+        //Debug.Log("Distance to Player: " + distance.ToString());
+        if (distance <= rangeAttackPlayer)
+        {
+            return EnemyState.ATTACK;
+        }
+        else if (distance <= rangeFindPlayer)
+        {
+            return EnemyState.PURSUE;
+        }
+        else if (distance <= rangeLosePlayer && state == EnemyState.PURSUE)
+        {
+            return EnemyState.PURSUE;
+        }
+        else
+        {
+            return EnemyState.IDLE;
+        }
     }
 
     public override void action_IDLE()
     {
-
+        Vector2 position = new Vector2(transform.position.x, transform.position.y);
+        float currentTime = Time.time;
+        if (currentTime - lastIdleFidgetTime > idleFidgetDelay)
+        {
+            Debug.Log("New Fidget");
+            lastIdleFidgetTime = currentTime;
+            idleFidgetDelay = Random.Range(idleFidgetDelayRange.x, idleFidgetDelayRange.y);
+            //Choose next targetPosition
+            while (true)
+            {
+                Vector2 nextFidgetDirection = Random.insideUnitCircle;
+                nextFidgetDirection.Normalize();
+                targetPosition = position + nextFidgetDirection * Random.Range(idleFidgetDistanceRange.x, idleFidgetDistanceRange.y);
+                if (Vector2.Distance(targetPosition, spawnPoint) <= maxDistanceFromSpawnPoint) break;
+            }
+        }
+        spriteRenderer.color = Color.HSVToRGB(0, 0.4f, 0.67f);
+        //Debug.Log("IDLE");
+        moveToTargetPosition();
     }
 
     public override void action_PURSUE()
     {
-
+        spriteRenderer.color = Color.HSVToRGB(0, 0.7f, 0.67f);
+        Debug.Log("PURSUE");
+        targetPosition = playerTransform.position;
+        moveToTargetPosition();
     }
 
     public override void action_ATTACK()
     {
+        spriteRenderer.color = Color.HSVToRGB(0, 1.0f, 0.67f);
+        Debug.Log("ATTACK");
+    }
 
+    void moveToTargetPosition()
+    {
+        Vector2 position = new Vector2(transform.position.x, transform.position.y);
+        Vector2 toTargetPosition = targetPosition - position;
+        if (toTargetPosition.magnitude > minFidgetThreshold)
+        {
+            toTargetPosition.Normalize();
+            GetComponent<Rigidbody2D>().velocity = toTargetPosition * MVSP;
+            Debug.DrawLine(position, targetPosition, Color.cyan);
+            Debug.Log(position.ToString() + ", " + targetPosition.ToString());
+        }
+        else
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            Debug.Log("Happy");
+        }
     }
 }
